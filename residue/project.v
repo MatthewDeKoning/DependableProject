4988d63c4f165aa5b681b248e054aaed76b240d8
Unknown <jiahan.jim.liu@gmail.com>
2017-12-06 20:19:37 -0600

    Two Rail 6.7-7.7

--- residue/project.v
+++ residue/project.v
@@ -9,89 +9,6 @@
 */
 /**************************************************************************/
 `define WIDTH 2
-/*
-module basic_tb();
-//residue test
-reg [3:0] A;
-reg [3:0] B;
-wire [1:0] O;
-wire [1:0] O2;
-wire [2:0] SUM;
-wire Cout;
-input_residue i1(A, B, O);
-three_addr a1({A[2], A[1], A[0]}, {B[2], B[1], B[0]}, SUM, Cout);
-residue r1({Cout,SUM}, O2);
-
-initial begin
-A = 0;
-B = 3; 
-#200
-A = 1;
-#200
-A = 2;
-#200
-A = 3;
-#200
-A = 4;
-#200
-A = 5;
-#200
-A = 6;
-#200
-A = 7;
-#200
-A = 0;
-B = 7;
-#200
-A = 1;
-#200
-A = 2;
-#200
-A = 3;
-#200
-A = 4;
-#200
-A = 5;
-#200
-A = 6;
-#200
-A = 7;
-#200
-$finish;
-end
-//Main test
-reg A0,A1,A2,B0,B1,B2,PAR,C0,C1,C2;
-wire X0,X1,X2,XC,XE0,XE1,Y0,Y1,Y2,YC,YE0,YE1;
-
-main m(A0,A1,A2,B0,B1,B2,PAR,C0,C1,C2,X0,X1,X2,XC,XE0,XE1,Y0,Y1,Y2,YC,YE0,YE1);
-
-initial begin
-A0 = 0;
-A1 = 1;
-A2 = 0;
-B0 = 0;
-B1 = 0;
-B2 = 0;
-PAR = 0;
-C0 = 1;
-C1 = 0;
-C2 = 0;
-#200
-B0 = 1;
-PAR = 1;
-C0 = 0;
-C1 = 1;
-#200
-B1 = 1;
-C1 = 0;
-C2 = 1;
-PAR = 0;
-#200
-$finish;
-end
-
-endmodule
-*/
 module main(A0,A1,A2,B0,B1,B2,PAR,C0,C1,C2,X0,X1,X2,XC,XE0,XE1,
             Y0,Y1,Y2,YC,YE0,YE1);
 
@@ -108,7 +25,8 @@ wire [`WIDTH:0] X;
 wire [1:0] YE;
 wire [1:0] XE;
 
-wire E1, E2, E3, E4, E5, E6;
+wire arg1_Error, arg2_Error;
+wire arg1_Error_N, arg2_Error_N;
 //wire E1, E2, E3, E4;
 wire carry1, carry2, carry3, carry4;
 //wire PA1, PA2, PC, PO;
@@ -132,6 +50,7 @@ wire [`WIDTH:0] OUT4;
 //wire [1:0] OR1;
 //wire [1:0] OR2;
 
+wire two_rail_FIRST, two_rail_FIRST_N, two_rail_SECOND, two_rail_SECOND_N;
 
 assign A = {A2, A1, A0};
 assign B = {B2, B1, B0};
@@ -146,31 +65,38 @@ codeword_detect cw2(A, B, {C2, C1, C0}, PAR, cw_error2);
 
 twos_comp tc1_FIRST(A, negative_A_FIRST);
 twos_comp tc2_FIRST(B, negative_B_FIRST);
-
 twos_comp tc1_SECOND(A, negative_A_SECOND);
 twos_comp tc2_SECOND(B, negative_B_SECOND);
 
 select_arg2 s1_FIRST({C2, C1, C0}, B, negative_B_FIRST, ARG2_FIRST);
 select_arg1 s2_FIRST({C2, C1, C0}, A, negative_A_FIRST, ARG1_FIRST);
-
 select_arg2 s1_SECOND({C2, C1, C0}, B, negative_B_SECOND, ARG2_SECOND);
 select_arg1 s2_SECOND({C2, C1, C0}, A, negative_A_SECOND, ARG1_SECOND);
 
-comp_three_bits arg_c1(ARG2_FIRST, ARG2_SECOND, E5);
-comp_three_bits arg_c2(ARG1_FIRST, ARG1_SECOND, E6);
+comp_three_bits arg_c1(ARG2_FIRST, ARG2_SECOND, arg2_Error);
+comp_three_bits arg_c2(ARG1_FIRST, ARG1_SECOND, arg1_Error);
+assign arg2_Error_N = ~arg2_Error;
+assign arg1_Error_N = ~arg1_Error;
 
 three_addr ta1(ARG1_FIRST, ARG2_FIRST, OUT1, carry1);
-three_addr ta2(ARG1_FIRST, ARG2_FIRST, OUT2, carry2);
+three_addr_complement ta2(ARG1_FIRST, ARG2_FIRST, OUT2, carry2);
 three_addr ta3(ARG1_SECOND, ARG2_SECOND, OUT3, carry3);
-three_addr ta4(ARG1_SECOND, ARG2_SECOND, OUT4, carry4);
+three_addr_complement ta4(ARG1_SECOND, ARG2_SECOND, OUT4, carry4);
 //three_addr ta3(ARG1_FIRST, ARG2_FIRST, OUT3, carry3);
 //three_addr ta4(ARG1_FIRST, ARG2_FIRST, OUT4, carry4);
 
-comp_four_bits c1(OUT1, OUT2, carry1, carry2, E1);
-comp_four_bits c2(OUT3, OUT4, carry3, carry4, E2);
+//comp_four_bits c1(OUT1, OUT2, carry1, carry2, E1);
+//comp_four_bits c2(OUT3, OUT4, carry3, carry4, E2);
 
-assign E3 = cw_error1 | E1 | E5;
-assign E4 = cw_error2 | E2 | E6;
+two_rail_tree tree_1(OUT1, OUT2, carry1, carry2, two_rail_FIRST, two_rail_FIRST_N);
+two_rail_tree tree_2(OUT3, OUT4, carry3, carry4, two_rail_SECOND, two_rail_SECOND_N);
+
+basic_two_rail(two_rail_FIRST, two_rail_FIRST_N, cw_error1, arg1_Error_N, YE0, YE1);
+basic_two_rail(two_rail_SECOND, two_rail_SECOND_N, cw_error2, arg2_Error_N, XE0, XE1);
+
+
+//assign E3 = cw_error1 | arg1_Error;
+//assign E4 = cw_error2 | arg2_Error;
 //assign E3 = cw_error | E1;
 //assign E4 = cw_error | E2;
 assign Y0 = OUT1[0];
@@ -196,8 +122,8 @@ parity_tree p3({C2, C1, C0}, PC);
 parity_tree p4({PA1,PA2, PC}, PO);
 */
 
-two_bit_two_one_mux m1(2'b11, 2'b01, E3, {YE1, YE0});
-two_bit_two_one_mux m2(2'b11, 2'b01, E4, {XE1, XE0});
+//two_bit_two_one_mux m1(2'b11, 2'b01, E3, {YE1, YE0});
+//two_bit_two_one_mux m2(2'b11, 2'b01, E4, {XE1, XE0});
 
 endmodule
 
@@ -214,7 +140,7 @@ assign out = one | two;
 endmodule
 /*************************************************************
 Three bit compare - one for error, zero for equal
-*/
+*************************************************************/
 module comp_three_bits(a, b, out);
 input [`WIDTH:0] a;
 input [`WIDTH:0] b;
@@ -230,7 +156,7 @@ endmodule
 
 /*************************************************************
 Four bit compare - one for error, zero for equal
-*/
+*************************************************************/
 module comp_four_bits(a, b, c1, c2, out);
 input [`WIDTH:0] a;
 input [`WIDTH:0] b;
@@ -249,12 +175,12 @@ endmodule
 
 /*************************************************************
 Return Two's Complement of a three bit number FIX
-*/
+*************************************************************/
 module twos_comp(a, out);
 input [`WIDTH:0] a;
 output [`WIDTH:0] out;
 
-wire [`WIDTH:0]negative;
+wire [`WIDTH:0] negative;
 wire cout;
 
 assign negative = ~a;
@@ -264,7 +190,7 @@ endmodule
 
 /*************************************************************
 if c[1] is high, negative b, else b
-*/
+*************************************************************/
 module select_arg2(c, a, a_neg, out);
 input [`WIDTH:0] a;
 input [`WIDTH:0] a_neg;
@@ -277,7 +203,7 @@ endmodule
 
 /*************************************************************
 if c[2] is high, negative a, else a
-*/
+*************************************************************/
 module select_arg1(c, a, a_neg, out);
 input [`WIDTH:0] a;
 input [`WIDTH:0] a_neg;
@@ -290,7 +216,7 @@ endmodule
 
 /*************************************************************
 Two bit two to one mux
-*/
+*************************************************************/
 module two_bit_two_one_mux(a, b, sel, out);
 input [1:0] a;
 input [1:0] b;
@@ -304,7 +230,7 @@ endmodule
 /*************************************************************
 Check that the input is valid
 sets e bit high if control or parity check fails
-*/
+*************************************************************/
 module codeword_detect(a, b, c, p, e);
 input [`WIDTH:0] a;
 input [`WIDTH:0] b;
@@ -322,7 +248,7 @@ assign e = ~(control & parity);
 endmodule
 /*************************************************************
 Check that C is valid - valid = 1
-*/
+*************************************************************/
 module control_check(c, o);
 input [`WIDTH:0] c;
 output o;
@@ -334,7 +260,7 @@ endmodule
 
 /*************************************************************
 Check the parity of A, B, and P - the parity bit, 1 = valid
-*/
+*************************************************************/
 module parity_check(a, b, p, o);
 input [`WIDTH:0] a;
 input [`WIDTH:0] b;
@@ -348,7 +274,7 @@ endmodule
 
 /*************************************************************
 Return parity of three bits
-*/
+*************************************************************/
 module parity_tree(a, p);
 
 input [`WIDTH:0] a;
@@ -361,7 +287,7 @@ endmodule
 
 /*************************************************************
 Compute three bit addition
-*/
+*************************************************************/
 module three_addr( a, b, s, cout );/* 8 bit ripple carry adder made up of 8 one_bit_adder */
 
 input [`WIDTH:0] a;
@@ -380,8 +306,33 @@ one_bit_adder a3(a[2],b[2],t2,s[2],cout);
 endmodule
 
 /*************************************************************
+Complement Compute three bit addition
+*************************************************************/
+module three_addr_complement( a, b, s, cout );/* 8 bit ripple carry adder made up of 8 one_bit_adder */
+
+input [`WIDTH:0] a;
+input [`WIDTH:0] b;
+
+
+output[`WIDTH:0] s;
+output           cout;
+
+wire t1,t2,t3,t4,t5,t6,t7;
+wire [`WIDTH:0] sum_orig;
+wire carry_orig;
+
+first_bit_adder a1(a[0],b[0],sum_orig[0],t1);
+one_bit_adder a2(a[1],b[1],t1,sum_orig[1],t2);
+one_bit_adder a3(a[2],b[2],t2,sum_orig[2],carry_orig);
+
+assign s[`WIDTH:0] = ~sum_orig[`WIDTH:0];
+assign cout = ~carry_orig;
+
+endmodule
+
+/*************************************************************
 One bit adder
-*/
+*************************************************************/
 module one_bit_adder(a0,b0,c0,s0,c1);
 
 /* three inputs which are 1 bit each */
@@ -398,8 +349,85 @@ assign c1 = (a0&b0)|(b0&c0)|(c0&a0);
 
 endmodule
 
+/*************************************************************
+Two bit adder
+*************************************************************/
+module two_bit_adder(A, B, S, Cin, Cout);
+input [1:0] A;
+input [1:0] B;
+input Cin;
+output [1:0] S;
+output Cout;
+wire C1;
 
+one_bit_adder a1(A[0], B[0], Cin, S[0], C1);
+one_bit_adder a2(A[1], B[1], C1, S[1], Cout);
 
+endmodule
+
+/*************************************************************
+First Bit Adder
+*************************************************************/
+module first_bit_adder(a0, b0, s0, c1);
+input a0;
+input b0;
+output s0;/* sum */
+output c1;/* carry out */
+
+assign s0 = a0^b0;
+assign c1 = a0&b0;
+endmodule
+
+/*************************************************************
+Basic Two Rails
+*************************************************************/
+module basic_two_rail(a, aN, b, bN, out, outN);
+input a;
+input aN;
+input b;
+input bN;
+output out;
+output outN;
+
+wire ab;
+wire aNbN;
+wire aNb;
+wire abN;
+
+assign ab = a&b;
+assign aNbN = aN&bN;
+assign aNb = aN&b;
+assign abN = a&bN;
+
+assign out = ab | aNbN;
+assign outN = aNb | abN;
+endmodule
+
+/*************************************************************
+Two Rails Tree
+*************************************************************/
+module two_rail_tree(a, b, c1, c2, out, outN);
+input [`WIDTH:0] a;
+input [`WIDTH:0] b;
+input c1;
+input c2;
+output out;
+output outN;
+
+wire intermediate_a;
+wire intermediate_aN;
+wire intermediate_b;
+wire intermediate_bN;
+
+basic_two_rail(a[0], b[0], a[1], b[1], intermediate_a, intermediate_aN);
+basic_two_rail(a[2], b[2], c1, c2, intermediate_b, intermediate_bN);
+
+basic_two_rail(intermediate_a, intermediate_aN, intermediate_b, intermediate_bN, out, outN);
+endmodule
+
+/*************************************************************
+input_residue
+*************************************************************/
 module input_residue(A, B, O);
 input [3:0] A;
 input [3:0] B;
@@ -415,6 +443,9 @@ two_bit_adder a1(AR, BR, RR, 1'b0, cout);
 residue r3({1'b0, cout, RR}, O);
 endmodule
 
+/*************************************************************
+residue
+*************************************************************/
 module residue(A, O);
 input [3:0] A;
 output [1:0] O;
@@ -429,29 +460,3 @@ two_bit_adder a2(output1, 2'b00, output2, cout, not_used);
 assign select = output2[0]&output2[1];
 assign O = (select)? 2'b00: output2;
 endmodule
-
-module two_bit_adder(A, B, S, Cin, Cout);
-input [1:0] A;
-input [1:0] B;
-input Cin;
-output [1:0] S;
-output Cout;
-wire C1;
-
-one_bit_adder a1(A[0], B[0], Cin, S[0], C1);
-one_bit_adder a2(A[1], B[1], C1, S[1], Cout);
-
-endmodule
-
-module first_bit_adder(a0, b0, s0, c1);
-input a0;
-input b0;
-output s0;/* sum */
-output c1;/* carry out */
-
-assign s0 = a0^b0;
-assign c1 = a0&b0;
-endmodule
-
-
-
